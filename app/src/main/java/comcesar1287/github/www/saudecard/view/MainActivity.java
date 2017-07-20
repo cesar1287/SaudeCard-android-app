1ece6ba53e3650bff84d3dffe42ab7c1b5707ab1
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-07-20 13:04:45 -0300

    Alterado nome do app e criado as pastas do MVP view e controller

--- app/src/main/java/comcesar1287/github/www/saudecard/view/MainActivity.java
+++ app/src/main/java/comcesar1287/github/www/saudecard/view/MainActivity.java
@@ -1,7 +1,9 @@
-package comcesar1287.github.www.saudecard;
+package comcesar1287.github.www.saudecard.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import comcesar1287.github.www.saudecard.R;
 
 public class MainActivity extends AppCompatActivity {
 
